2a87f9b5005270eca21067ad1399b4059cb8186e
Sulay Shah <sulay15399@gmail.com>
2021-11-28 17:32:57 -0700

    added task2 and decision tree

---
+++ Submission/Code/scripts/SulayShah/task2.bat
@@ -0,0 +1,14 @@
+set training_images_folder_path="../../../1000"
+set feature_model=ELBP
+set dimensionality_reduction_technique=PCA
+set reduced_dimensions_count=10
+set test_images_folder_path="../../../1000"
+set classifier=DT
+
+python src/tasks/Task2.py ^
+--training_images_folder_path %training_images_folder_path% ^
+--feature_model %feature_model% ^
+--dimensionality_reduction_technique %dimensionality_reduction_technique% ^
+--reduced_dimensions_count %reduced_dimensions_count% ^
+--classifier %classifier% ^
+--test_images_folder_path %test_images_folder_path%
